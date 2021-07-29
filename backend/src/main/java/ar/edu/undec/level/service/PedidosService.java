af43a561aff2ad02e22cd6d25cea4a4d77b67b05
juan asis <juan.asis1234@gmail.com>
2021-07-29 17:05:17 -0300

    se agrego la fecha pero falta formatear para guardar en la bd
    la funcion nuevaFecha debe devolver la fecha en el formato yyyy-dd-mm hh-mm-ss

--- backend/src/main/java/ar/edu/undec/level/service/PedidosService.java
+++ backend/src/main/java/ar/edu/undec/level/service/PedidosService.java
@@ -5,9 +5,11 @@ import ar.edu.undec.level.controller.dto.Response;
 import ar.edu.undec.level.storage.entity.EstadoPedido;
 import ar.edu.undec.level.storage.entity.Pedido;
 import ar.edu.undec.level.storage.repository.PedidosRepository;
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -18,11 +20,13 @@ public class PedidosService {
 
     public Response save(PedidoRequest pedidos) {
         Response response = new Response();
+        System.out.println(pedidos.getIdMesa()+" "+pedidos.getIdMozo()+" "+pedidos.getIdProducto());
         Pedido entity = new Pedido();
         entity.setIdMozo(pedidos.getIdMozo());
         entity.setIdMesa(pedidos.getIdMesa());
         entity.setIdProducto(pedidos.getIdProducto());
         entity.setEstado(EstadoPedido.ENCOLA);
+        entity.setFecha(nuevaFecha());
         pedidosRepo.save(entity);
         response.setData("guardado");
         return response;
@@ -30,11 +34,15 @@ public class PedidosService {
 
     public Response findAll() {
         Response  response = new Response();
-        List<Pedido> pedidosList = pedidosRepo.findAll();
-        /*if(pedidosList.isEmpty())
+        List<Pedido> pedidoList = pedidosRepo.findAll();
+        if(pedidoList.isEmpty())
             response.setMessage("No hay Pedidos");
-        else */
-        response.setData(pedidosList);
+        else
+        response.setData(pedidoList);
         return response;
+    }
+    private Date nuevaFecha(){
+        System.out.println(new Date());
+        return new Date();
     }
 }
