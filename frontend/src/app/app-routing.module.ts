21f5413049b53c68d8d0cb4920449073995f9545
juanasis <38094379+juanasis@users.noreply.github.com>
2021-08-06 19:37:09 -0300

    nav bar correccion de path en routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CajerosComponent } from './pages/cajeros/cajeros.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { CocinerosComponent } from './pages/cocineros/cocineros.component';
+import { MozosComponent } from './pages/mozos/mozos.component';
 
-const routes: Routes = [];
+const routes: Routes = [  {
+  path: 'cajeros',
+  component: CajerosComponent
+},
+{
+  path: 'mozos',
+  component: MozosComponent
+},
+{
+  path: 'cocineros',
+  component: CocinerosComponent
+},
+{
+  path: 'clientes',
+  component: ClientesComponent
+},
+{
+  path: '**',
+  redirectTo: 'Inicio'
+} ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/models/producto.ts
@@ -0,0 +1,9 @@
+export class Pedido {
+    id: number;
+    nombre: String;
+    descripcion: String;
+    cantidad: number;
+    categoria: String;
+    precio: number;
+     
+}
