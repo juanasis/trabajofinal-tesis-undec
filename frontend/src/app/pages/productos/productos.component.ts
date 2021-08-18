41e14cc875f0f27ab160360ed35fcbdfec70bad0
juan asis <juan.asis1234@gmail.com>
2021-08-18 20:09:39 -0300

    listado de productos

--- frontend/src/app/pages/productos/productos.component.ts
+++ frontend/src/app/pages/productos/productos.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+
+
 
 @Component({
   selector: 'app-productos',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductosComponent implements OnInit {
 
-  constructor() { }
+  productosArray: Producto[];
+  bebidasArray: Producto[];
+  oldproducto: Producto = new Producto();
+  selectedProducto: Producto = new Producto();
 
+  constructor(private http: HttpClient) { }
+
+  listarBebidas(){
+    this.productosArray.forEach(element => {
+      if (element.categoria = 'bebidas') {
+        this.bebidasArray.push(element);        
+      }
+      console.log("click");
+    });
+    
+  }
   ngOnInit(): void {
+    this.http.get("http://localhost:8080/productos",{responseType: 'json'}).subscribe(
+      (resp:any) =>{
+      this.productosArray = resp.data;
+       console.log(this.productosArray); })
+
   }
 
 }
