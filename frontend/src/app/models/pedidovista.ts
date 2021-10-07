2fe2ef593a310c9d4a1a49bec1a44cf1c16130b8
juan asis <juan.asis1234@gmail.com>
2021-10-06 22:49:54 -0300

    cantidad de productos en pedido incementa correctamente

--- frontend/src/app/models/pedidovista.ts
+++ frontend/src/app/models/pedidovista.ts
@@ -13,15 +13,15 @@ export class Pedidovista {
         let productoVista = new ProductoVista(producto, 1);
         //let indice = this.productos.indexOf(productoVista)
         let indice = this.productos.findIndex( s => s.producto.nombre == productoVista.producto.nombre )
-        console.log("indice: " + indice); 
+        
        
         if(indice === -1){
               this.productos.push(productoVista);          
           }else{
-              console.log("dentro del else");
-              this.productos[indice].cantidad += 1; 
+              
+              this.productos[indice].producto.cantidad ++;
+               
           }
-          console.log("cantidad de productos: " + (this.productos.length)); 
-          console.log("cantidad de tragos: " + (this.productos[0].producto.cantidad)); 
+        
     }
 }
