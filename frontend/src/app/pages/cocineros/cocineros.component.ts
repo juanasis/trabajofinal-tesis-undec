26929a347e94d14dea94fa536584962dcb8e42a2
juan asis <juan.asis1234@gmail.com>
2021-10-13 17:57:03 -0300

    cocineros muestra el detalle del pedido
    mozo mock, mesa mock, minutos mock, botones de cambio de estado mock

--- frontend/src/app/pages/cocineros/cocineros.component.ts
+++ frontend/src/app/pages/cocineros/cocineros.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
+import { Pedido } from 'src/app/models/pedido';
+import { HttpClient } from '@angular/common/http';
+import { PedidoDTO } from 'src/app/models/pedidoDTO';
 @Component({
   selector: 'app-cocineros',
   templateUrl: './cocineros.component.html',
   styleUrls: ['./cocineros.component.css']
 })
 export class CocinerosComponent implements OnInit {
+  pedidos: PedidoDTO[] = [];
+  
 
-  constructor() { }
-
-  ngOnInit(): void {
+   
+    constructor(private http: HttpClient) { }
+    ngOnInit(): void {
+      this.http.get("http://localhost:8080/pedidos",{responseType: 'json'}).subscribe(
+        (resp:any) =>{
+       
+        this.pedidos = resp.data;
+        console.log(this.pedidos);
+         
+         
+          })
   }
 
 }
